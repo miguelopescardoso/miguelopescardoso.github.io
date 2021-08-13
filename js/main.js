cf50e7a44d3ef26685ceb4245db03a1c3e822703
DrP1nkEye5 <miguel.lopescardoso2006@gmail.com>
2021-08-13 23:42:40 +0200

    commit2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
+var $menuWidth = 192;
+var $open = true;
 function menu(){
-    console.log("Menu opened");
-    gsap.to('menuExpand', {duration:0.5, right:right})
+    if ($open == true){
+        console.log("Menu opened");
+        gsap.to('#menuExpand', {left:1855 - $menuWidth});
+        $open = false;
+    }
+    else if($open == false){
+        console.log("Menu closed");
+        gsap.to('#menuExpand', {left:1855+$menuWidth});
+        $open = true;
+    }
+}
+
+function fillEnt(){
+    gsap.to(document.querySelector("#art"), {duration:0.7, fill:"#E21C41"});
+}
+
+function fillLea(){
+    gsap.to(document.querySelector("#art"), {duration:0.7, fill:"white"});
 }
