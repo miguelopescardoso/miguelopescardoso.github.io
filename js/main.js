a49ae35992674fc444818579aab6e09eea87a371
DrP1nkEye5 <miguel.lopescardoso2006@gmail.com>
2021-08-14 16:39:48 +0200

    v2.0

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function logoFillEnt(){
+    gsap.to(document.querySelector("#AiArtLogo"), {duration:0.7, fill:"#E21C41"});
+}
+
+function logoFillLea(){
+    gsap.to(document.querySelector("#AiArtLogo"), {duration:0.7, fill:"white"});
+}
