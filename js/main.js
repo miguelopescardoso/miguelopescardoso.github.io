e04d8ae248f389b993601f985ae22f4668abfdfd
DrP1nkEye5 <miguel.lopescardoso2006@gmail.com>
2021-08-14 16:38:52 +0200

    new files

--- js/main.js
+++ js/main.js
@@ -1,22 +1,7 @@
-var $menuWidth = 192;
-var $open = true;
-function menu(){
-    if ($open == true){
-        console.log("Menu opened");
-        gsap.to('#menuExpand', {left:1855 - $menuWidth});
-        $open = false;
-    }
-    else if($open == false){
-        console.log("Menu closed");
-        gsap.to('#menuExpand', {left:1855+$menuWidth});
-        $open = true;
-    }
+function logoFillEnt(){
+    gsap.to(document.querySelector("#AiArtLogo"), {duration:0.7, fill:"#E21C41"});
 }
 
-function fillEnt(){
-    gsap.to(document.querySelector("#art"), {duration:0.7, fill:"#E21C41"});
-}
-
-function fillLea(){
-    gsap.to(document.querySelector("#art"), {duration:0.7, fill:"white"});
+function logoFillLea(){
+    gsap.to(document.querySelector("#AiArtLogo"), {duration:0.7, fill:"white"});
 }
