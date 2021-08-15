7e81e954bb515fa6fa6b9dc507f886da3a953f75
DrP1nkEye5 <miguel.lopescardoso2006@gmail.com>
2021-08-15 23:12:46 +0200

    AiArt.v3.0

--- js/main.js
+++ js/main.js
@@ -5,3 +5,43 @@ function logoFillEnt(){
 function logoFillLea(){
     gsap.to(document.querySelector("#AiArtLogo"), {duration:0.7, fill:"white"});
 }
+
+function fillShowroomEnt(){
+    gsap.to(document.querySelector('#showroomText'), {duration: 0.7, color:"#E21C41"});
+}
+
+function fillShowroomLea(){
+    gsap.to(document.querySelector('#showroomText'), {duration: 0.7, color:"white"});
+}
+
+function fillInstEnt(){
+    gsap.to(document.querySelector("#insta"), {duration:0.7, fill:"#E21C41"});
+}
+
+function fillInstLea(){
+    gsap.to(document.querySelector("#insta"), {duration:0.7, fill:"white"});
+}
+
+function fillFaceEnt(){
+    gsap.to(document.querySelector("#face"), {duration:0.7, fill:"#E21C41"});
+}
+
+function fillFaceLea(){
+    gsap.to(document.querySelector("#face"), {duration:0.7, fill:"white"});
+}
+
+function fillTwitEnt(){
+    gsap.to(document.querySelector("#twit"), {duration:0.7, fill:"#E21C41"});
+}
+
+function fillTwitLea(){
+    gsap.to(document.querySelector("#twit"), {duration:0.7, fill:"white"});
+}
+
+function fillYoutEnt(){
+    gsap.to(document.querySelector("#yout"), {duration:0.7, fill:"#E21C41"});
+}
+
+function fillYoutLea(){
+    gsap.to(document.querySelector("#yout"), {duration:0.7, fill:"white"});
+}
